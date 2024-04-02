3574ae3bd5dda5ba61e1fcd2441db3663ef45526
Radifan <radifan.fariz@serbamulia.co.id>
2024-04-02 16:06:07 +0700

    feat: add filter and filterColumns params in moduleJoinAccess api

--- controllers/moduleJoinAccessController.go
+++ controllers/moduleJoinAccessController.go
@@ -23,11 +23,11 @@ type ModuleMetadataJoinAccssedDataPagination struct {
 func ModuleMetadataJoinAccessDataPaginate(value interface{}, pagination *ModuleMetadataJoinAccssedDataPagination, db *gorm.DB, ctx *gin.Context) func(db *gorm.DB) *gorm.DB {
 	if pagination.GradeID != 0 || pagination.UserID != 0 || pagination.PositionID != 0 || pagination.CompanyID != 0 {
 		return func(db *gorm.DB) *gorm.DB {
-			return db.Offset(pagination.GetOffset()).Limit(pagination.GetLimit()).Order(pagination.GetSort()).Table("t_module_metadata").Joins("left join t_access_data on t_access_data.n_module_meta_id = t_module_metadata.n_id").Where("? = ANY(n_array_grade_id) OR ? = ANY(n_array_user_id) OR ? = ANY(n_array_position_id) OR ? = ANY(n_array_company_id)", pagination.GradeID, pagination.UserID, pagination.PositionID, pagination.CompanyID)
+			return db.Offset(pagination.GetOffset()).Limit(pagination.GetLimit()).Order(pagination.GetSort()).Table("t_module_metadata").Joins("left join t_access_data on t_access_data.n_module_meta_id = t_module_metadata.n_id").Where(pagination.GetFilterColumn()+" ILIKE ? AND "+"( ? = ANY(n_array_grade_id) OR ? = ANY(n_array_user_id) OR ? = ANY(n_array_position_id) OR ? = ANY(n_array_company_id))", "%"+pagination.GetFilter()+"%", pagination.GradeID, pagination.UserID, pagination.PositionID, pagination.CompanyID)
 		}
 	}
 	return func(db *gorm.DB) *gorm.DB {
-		return db.Offset(pagination.GetOffset()).Limit(pagination.GetLimit()).Order(pagination.GetSort())
+		return db.Offset(pagination.GetOffset()).Limit(pagination.GetLimit()).Order(pagination.GetSort()).Where(pagination.GetFilterColumn()+" ILIKE ? ", "%"+pagination.GetFilter()+"%")
 	}
 }
 
@@ -36,6 +36,14 @@ func ModuleMeatadataJoinAccessDataFindPaging(ctx *gin.Context) {
 	limit, _ := strconv.Atoi(ctx.Query("per_page"))
 	page, _ := strconv.Atoi(ctx.Query("page"))
 	sort := ctx.Query("sort")
+	var filterColumn string = "c_learning_journey"
+	if ctx.Query("filter_columns") != "" {
+		filterColumn = ctx.Query("filter_columns")
+	}
+	var filter string
+	if ctx.Query("filter") != "" {
+		filter = ctx.Query("filter")
+	}
 	var gradeId int
 	if ctx.Query("grade_id") != "" {
 		gradeId, _ = strconv.Atoi(ctx.Query("grade_id"))
@@ -55,9 +63,11 @@ func ModuleMeatadataJoinAccessDataFindPaging(ctx *gin.Context) {
 
 	params := ModuleMetadataJoinAccssedDataPagination{
 		Pagination: &utils.Pagination{
-			Limit: limit,
-			Page:  page,
-			Sort:  sort,
+			Limit:        limit,
+			Page:         page,
+			Sort:         sort,
+			FilterColumn: filterColumn,
+			Filter:       filter,
 		},
 		GradeID:    gradeId,
 		UserID:     userId,
@@ -66,9 +76,11 @@ func ModuleMeatadataJoinAccessDataFindPaging(ctx *gin.Context) {
 	}
 	paramsNoPageNoLimit := ModuleMetadataJoinAccssedDataPagination{
 		Pagination: &utils.Pagination{
-			Limit: -1,
-			Page:  0,
-			Sort:  sort,
+			Limit:        -1,
+			Page:         0,
+			Sort:         sort,
+			FilterColumn: filterColumn,
+			Filter:       filter,
 		},
 		GradeID:    gradeId,
 		UserID:     userId,
@@ -80,8 +92,8 @@ func ModuleMeatadataJoinAccessDataFindPaging(ctx *gin.Context) {
 	res := initializers.DB.Scopes(ModuleMetadataJoinAccessDataPaginate(moduleMetadata, &params, initializers.DB, ctx)).Find(&moduleMetadata)
 
 	/* for total actual total data not total data per page */
-	var moduleMetadataNoPageNoLimit []models.ModuleMetadata
-	resNoPageNoLimit := initializers.DB.Scopes(ModuleMetadataJoinAccessDataPaginate(moduleMetadata, &paramsNoPageNoLimit, initializers.DB, ctx)).Find(&moduleMetadataNoPageNoLimit)
+	var moduleMetadataNoLimit []models.ModuleMetadata
+	resNoPageNoLimit := initializers.DB.Scopes(ModuleMetadataJoinAccessDataPaginate(moduleMetadata, &paramsNoPageNoLimit, initializers.DB, ctx)).Find(&moduleMetadataNoLimit)
 
 	totalRows := resNoPageNoLimit.RowsAffected
 	params.TotalData = totalRows
